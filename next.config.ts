e966c7ad13ea7a1e6e6ff32227bb0b64a67768cb
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-06-23 11:56:58 +0200

    fix: remove unused config options from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
-  reactStrictMode: false,
 };
 
 export default nextConfig;
